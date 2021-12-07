c28555a1ee4101c90a96002cc598e7923babb32c
namdlefhtes <namdlefhtes@gmail.com>
2021-12-07 09:00:08 -0500

    valuies coming through

--- script.js
+++ script.js
@@ -2,12 +2,11 @@
 //working api call format:
 //https://sheets.googleapis.com/v4/spreadsheets/1dJ0MP5z_5UG5bFfAKa17ksPRKKy9taVEet7W5-2ZRhY?key=AIzaSyBwr2UeuNOcdWJ8qB5HqHA6eE_i6iLTc74
 //https://console.cloud.google.com/home/dashboard?project=bill-bolton
-
 async function checkSheet() {
 
 const sheetId = `1dJ0MP5z_5UG5bFfAKa17ksPRKKy9taVEet7W5-2ZRhY`;
 const googleAPI = `https://sheets.googleapis.com`;
-const range = `Team 4`;
+const range = `Team4`;
 const googleGET = `/v4/spreadsheets/${sheetId}/values/${range}`;
 const apiKey= `AIzaSyBwr2UeuNOcdWJ8qB5HqHA6eE_i6iLTc74`;
 
@@ -19,9 +18,15 @@ const resp = await fetch(sheetData, {mode:`cors`});
 
 // console.log(resp.url);
 console.log(resp.url);
-// need to run another get on the data bove to access contents.
+let respURL = resp.url;
+let respJSON = await resp.json();
+// getting values.
+console.log(respJSON.values);
+
+
+
 body.innerHTML = `<a href=${resp.url}>link</a>`;
-console.log(respJSON);
+// console.log(respJSON);
 
 };
 
