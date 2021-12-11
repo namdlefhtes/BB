f4ceda30987f74ef1ffe0938f3b9f4f43b59fb43
namdlefhtes <namdlefhtes@gmail.com>
2021-12-11 10:48:34 -0500

    not getting errors now, but still not writing to rows

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //https://developers.google.com/sheets/api/reference/rest
 //working api call format:
 //https://sheets.googleapis.com/v4/spreadsheets/1dJ0MP5z_5UG5bFfAKa17ksPRKKy9taVEet7W5-2ZRhY?key=AIzaSyBwr2UeuNOcdWJ8qB5HqHA6eE_i6iLTc74
+
 //https://console.cloud.google.com/home/dashboard?project=bill-bolton
 const table = document.getElementById("teamDisplay");
 
@@ -8,7 +9,10 @@ const elBuilder = (el, values, appendTo, id) => {
     let newEl = document.createElement(el);
     newEl.innerHTML = values;
     newEl.id = id;
-    appendTo.appendChild(newEl);
+    
+    console.log("obj detected " +appendTo );
+    appendTo.append(newEl);
+    
 };
 
 async function checkSheet() {
@@ -21,14 +25,11 @@ const apiKey= `AIzaSyBwr2UeuNOcdWJ8qB5HqHA6eE_i6iLTc74`;
 
 //https://developers.google.com/sheets/api/guides/concepts
 const sheetData = `${googleAPI}${googleGET}?key=${apiKey}`;
-const body = document.getElementsByTagName("body")[0];
-
 const resp = await fetch(sheetData, {mode:`cors`});
-let respURL = resp.url;
 let respJSON = await resp.json();
 // getting values.
 console.log(respJSON.values);
-console.log(respJSON.values.length);
+
 const buildTable = () => {
     //builds table title and headings
     // element, values, appendto, id
@@ -37,16 +38,33 @@ const buildTable = () => {
     elBuilder("th", respJSON.values[0][0], row1, "head1");
     elBuilder("th", respJSON.values[0][1], row1, "head2");
     
-    //respJSON.values[1] and onwards is a player.
-        //loop 1
-        for (let i=1; i < respJSON.values.length; i++){
-            let row = `row${i+1}`;
-            // el, txt, append to, rowNum
-            elBuilder("tr","", thead, row);
-            for (let j=0; j < 2; j++) {
-                elBuilder("td", respJSON.values[i][j], row,"");
-                console.log(respJSON.values[i][j] +row);
+    // //respJSON.values[1] and onwards is a player.
+    //     //loop 1
+        // starts on row 2
+        let i=2
+        while ( i < respJSON.values.length) {
+            console.log(i);
+            // el, txt, append to, rowNum. this part works.
+            elBuilder("tr","", thead, `row${i}`);
+
+            let j=0
+            while (j < 2) { 
+                let row = "row"+i;     
+                try {
+                    let rowObj = JSON.parse(row);
+                    elBuilder("td", respJSON.values[i][j], rowObj,"");
+                    elBuilder("td", respJSON.values[i][j], rowObj,"");
+                    j++;
+
+                } catch (err) {
+                    console.log("parsing error");
+                    return undefined; 
+                }
+                // for some reason row2 != `row${rowNum}`
+              
+
             };
+            i++;
         };
     // each player has a number [0] and name [1]
         //loop2 within loop1 
@@ -66,7 +84,7 @@ const buildTable = () => {
     // elBuilder("tr","", thead, "row4");
     // elBuilder("td", respJSON.values[3][0], row4, "");
     // elBuilder("td", respJSON.values[3][1], row4, "");
-}
+};
 
 buildTable(respJSON);
 
