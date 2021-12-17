811f85a58aebd5cae240e5f649a2db5548071291
namdlefhtes <namdlefhtes@gmail.com>
2021-12-17 09:02:36 -0500

    working team list

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # BB
+
+//https://developers.google.com/sheets/api/reference/rest
+//working api call format:
+//https://sheets.googleapis.com/v4/spreadsheets/1dJ0MP5z_5UG5bFfAKa17ksPRKKy9taVEet7W5-2ZRhY?key=AIzaSyBwr2UeuNOcdWJ8qB5HqHA6eE_i6iLTc74
+
+//https://console.cloud.google.com/home/dashboard?project=bill-bolton
+// const table = document.getElementById("teamDisplay");
--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-//https://developers.google.com/sheets/api/reference/rest
-//working api call format:
-//https://sheets.googleapis.com/v4/spreadsheets/1dJ0MP5z_5UG5bFfAKa17ksPRKKy9taVEet7W5-2ZRhY?key=AIzaSyBwr2UeuNOcdWJ8qB5HqHA6eE_i6iLTc74
-
-//https://console.cloud.google.com/home/dashboard?project=bill-bolton
-// const table = document.getElementById("teamDisplay");
-
 const container = document.getElementById("container");
-const teamArray = [];
 const teamList = ["team1","team2","team3","team4", "team5", "team6"];
 
 const elBuilder = (el, values, appendTo, id) => {
@@ -25,8 +17,7 @@ const elBuilder = (el, values, appendTo, id) => {
         newEl.innerHTML = values;
         newEl.id = id;
         appendTo.appendChild(newEl);
-    }
-    
+    }  
 };
 
 async function checkSheet(team) {
@@ -45,16 +36,15 @@ const buildTable = (respJSON, team) => {
     // console.log(respJSON.values);
   
     //builds table title and headings
-    elBuilder("table","", container, team)
+    elBuilder("table","", container, team);
    
-    // element, values, appendto, id
     // creates thead
     elBuilder("thead", "", team, `${team}-tHead`);
     // creates first table row
     elBuilder("tr","", `${team}-tHead`, `${team}-row1`);
     // creates 2 table headings
-    elBuilder("th", "",`${team}-row1`, "" );
-    elBuilder("th", respJSON.values[0][2],`${team}-row1`, "teamName" );
+    elBuilder("th", "",`${team}-row1`, "", "");
+    elBuilder("th", respJSON.values[0][2],`${team}-row1`, "teamName");
     
         // starts on row 2
         let i=2;
@@ -69,26 +59,14 @@ const buildTable = (respJSON, team) => {
             };
             i++;
         };
+    // add order styling    
+    let teamEl = document.getElementById(team);
+    teamEl.style.order = team.slice(-1);
 };
 
-// store respJSON in an array of objects, arrange, by order, then build table.
-  // store in array first
-  teamArray.push(respJSON.values);
-  // access part of array with team name
-//   console.log(teamArray[0][0][2]);
-// sort by team name in array of arrays.
-teamArray.sort();
-// this currently receives data asynchrounously
-
-// teamArray.forEach(teamData =>
-//     //either build table needs to change or data in array does to meet
-//     //current requirements.  
-//     buildTable(teamData, teamList[0])
-
-//     );
-
 buildTable(respJSON, team);
-// return team;
+
+return team;
 };
 // this runs early. loop to check all teams?
 const checkPromise = new Promise ((resolve, reject) =>{
@@ -96,12 +74,11 @@ const checkPromise = new Promise ((resolve, reject) =>{
     teamList.forEach((teamList) => {
     checkSheet(teamList)
         .then((result) => {
-        console.log(`returned ${result}`);
+        // console.log(`returned ${result}`);
         resolve(result);
         })
         .catch(error => {
             reject(error);
         });
     })    
-
 });
